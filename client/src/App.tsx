4f82891ea05e53995d2978f4b774034ddcd98655
Nibedan Pati <nibedanpati@gmail.com>
2026-08-09 22:28:17 +0530

    project configuration

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-amber-300 text-5xl">Vite + React</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
